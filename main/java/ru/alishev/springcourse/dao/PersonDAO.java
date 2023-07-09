2a8a64867fd57e5d96feab1c220163178ac99497
Elvin Abaev <elvinabaev@gmail.com>
2023-07-09 04:42:12 +0300

    Introduction in CRUD. Using pattern DAO(Data Access Object) in Spring MVC.

---
+++ main/java/ru/alishev/springcourse/dao/PersonDAO.java
@@ -0,0 +1,30 @@
+package ru.alishev.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ru.alishev.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Elvin"));
+        people.add(new Person(++PEOPLE_COUNT, "Ramis"));
+        people.add(new Person(++PEOPLE_COUNT, "Chloe"));
+        people.add(new Person(++PEOPLE_COUNT, "Kara"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
