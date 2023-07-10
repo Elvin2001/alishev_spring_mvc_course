72b4bfde4a5a51de00994eb44083e2b07ad6c38e
Elvin Abaev <elvinabaev@gmail.com>
2023-07-10 04:18:16 +0300

    Annotation @ModelAttribute. HTML forms in Thymeleaf.

--- main/java/ru/alishev/springcourse/controllers/PeopleController.java
+++ main/java/ru/alishev/springcourse/controllers/PeopleController.java
@@ -2,10 +2,9 @@ package ru.alishev.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.alishev.springcourse.dao.PersonDAO;
+import ru.alishev.springcourse.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -28,5 +27,16 @@ public class PeopleController {
         // Получим одного человека по id из DAO и передадим на отображение в представление
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
